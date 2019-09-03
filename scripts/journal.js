bb268e0d598082cc46e8513e533a3b5c85fee65c
Chandler Turner <chandlerturner@Chandlers-MacBook-Air.local>
2019-09-03 08:55:26 -0500

    Daily Journal Part 2

---
+++ scripts/journal.js
@@ -0,0 +1,16 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const journalEntry = {
+    date: "",
+    conceptsCovered: "",
+    contents: "",
+    mood: "",
+}
+
+const entryArray = [
+
+]
+
+entryArray.push(journalEntry);
